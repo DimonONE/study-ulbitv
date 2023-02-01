59bed4beeaad70a61da9b29af4844f1d95bf128b
DimonONE <dimonmezey@gmail.com>
2023-02-01 18:23:26 +0200

    helpers classNames

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,13 @@ import "./styles/index.scss";
 import { Suspense } from "react";
 import { MainPgeAsync } from "./pages/MainPage/MainPage.async";
 import { useTheme } from "./theme/useTheme";
+import { classNames } from "./helpers/classNames/classNames";
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <div className={`app ${theme}`}>
+    <div className={classNames("app", { selected: true }, [theme])}>
       <button onClick={toggleTheme}>{theme}</button>
 
       <Link to="/" className="nav">
@@ -19,6 +20,7 @@ const App = () => {
       <Link to="/about" className="nav">
         AboutPage
       </Link>
+
       <Suspense fallback={<div>Загрузка...</div>}>
         <Routes>
           <Route path="/" element={<MainPgeAsync />} />
---
+++ src/helpers/classNames/classNames.ts
@@ -0,0 +1,13 @@
+export function classNames(
+  cls: string,
+  mods: Record<string, boolean | string>,
+  additional: string[]
+): string {
+  return [
+    cls,
+    ...additional,
+    Object.entries(mods)
+      .filter(([, value]) => Boolean(value))
+      .map(([className]) => className),
+  ].join(" ");
+}
