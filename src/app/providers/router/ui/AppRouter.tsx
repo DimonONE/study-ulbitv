d5b582b5ac42085803a1894cc3dff05dd1e4c2e4
DimonONE <dimonmezey@gmail.com>
2023-02-05 18:33:33 +0200

    fix: route

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -7,7 +7,7 @@ const AppRouter = () => {
     <Suspense fallback={<div>Загрузка...</div>}>
       <Routes>
         {Object.values(routeConfig).map(({ path, element }) => (
-          <Route key={path} path={path} element={element}></Route>
+          <Route key={path} path={path} element={element} />
         ))}
       </Routes>
     </Suspense>
