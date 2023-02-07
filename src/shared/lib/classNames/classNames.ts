9bd46e2a136bb27bebee779eee030e7df8d804e1
DimonONE <dimonmezey@gmail.com>
2023-02-07 12:28:20 +0200

    fix : helper classNames

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -1,11 +1,13 @@
+type Mods = Record<string, boolean | string>;
+
 export function classNames(
   cls: string,
-  mods?: Record<string, boolean | string>,
-  additional?: string[]
+  mods: Mods = {},
+  additional: string[] = []
 ): string {
   return [
     cls,
-    ...additional,
+    ...additional.filter(Boolean),
     Object.entries(mods)
       .filter(([, value]) => Boolean(value))
       .map(([className]) => className),
